a914bdb7b33b3a36548767905bc02889c7d231f1
Wes White <wtealwhite@gmail.com>
2021-04-15 15:00:46 -0600

    initial

---
+++ components/Nav/Nav.tsx
@@ -0,0 +1,29 @@
+import Link from "next/link";
+import { FaShoppingCart } from "react-icons/fa";
+//@ts-ignore
+import styles from "./Nav.module.css";
+
+import { useCart } from "../../hooks/use-cart";
+
+const Nav = () => {
+  //@ts-ignore
+  const { subtotal } = useCart();
+  return (
+    <nav className={styles.nav}>
+      <p className={styles.navTitle}>
+        <Link href="/">
+          <a>Sideling Hill Hackle</a>
+        </Link>
+      </p>
+      <p className={styles.navCart}>
+        <Link href="/cart">
+          <a>
+            <FaShoppingCart /> ${subtotal.toFixed(2)}
+          </a>
+        </Link>
+      </p>
+    </nav>
+  );
+};
+
+export default Nav;
